29fd67ce3de08e57805f96e32124d3a9762cf025
s-damasceno <samueldamasceno.vip@gmail.com>
2024-07-12 02:14:26 -0300

    função de editar imagem adicionada

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from apps.galeria.models import Fotografia
 from django.contrib import messages
+from apps.galeria.forms import FotografiaForm
 
 def index(request):
     if not request.user.is_authenticated:
         messages.error(request, 'Usuário não logado')
         return redirect('login')
     
-    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
     return render(request, 'galeria/index.html', {"fotografias": fotografias})
 
 def imagem(request, foto_id):
@@ -15,7 +16,7 @@ def imagem(request, foto_id):
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
 def buscar(request):
-    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
 
     if "buscar" in request.GET:
         nome_a_buscar = request.GET['buscar']
@@ -23,3 +24,41 @@ def buscar(request):
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
     return render(request, "galeria/buscar.html", {"cards": fotografias})
+
+def nova_imagem(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+
+    form = FotografiaForm()
+    
+    if request.method == 'POST':
+        form = FotografiaForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Fotografia cadastrada com sucesso')
+            return redirect('index')
+    
+    return render(request, 'galeria/nova_imagem.html', {'form': form})
+
+def editar_imagem(request, foto_id):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    
+    fotografia = Fotografia.objects.get(id=foto_id)
+    form = FotografiaForm(instance=fotografia)
+
+    if request.method == 'POST':
+        form = FotografiaForm(request.POST, request.FILES, instance=fotografia)
+        
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Fotografia editada com sucesso')
+            return redirect('index')
+    
+    return render(request, 'galeria/editar_imagem.html', {'form': form, 'foto_id': foto_id})
+
+def deletar_imagem(request):
+    pass
