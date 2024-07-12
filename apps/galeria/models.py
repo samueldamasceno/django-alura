b3267ee3eee4667f01210508d352fdbdd0e7f7e4
Wesley Ramos <wesley.rl1997@gmail.com>
2024-07-12 11:12:44 -0300

    Update models.py
    Fala meu querido tudo bem!? Sou Engenheiro Senior de Django, pode me chamar no linkedin se precisar!, hoje estou dando uma olhada em alguns projetos para ajudar :);
    
    Fiz uma pequena alteração no seu codigo, retirei o null; dos charfield e do textfield, o motivo é que hoje como boa prática no Django usamos apenas o blank=True nesses casos, como queremos um retorno de apenas "texto"
    
    Null é considerado outro tipo de valor e tratado como "objeto" em alguns casos, então como boa prática colocamos apenas blank=True 
    
    Espero ter ajudado! :)

--- apps/galeria/models.py
+++ apps/galeria/models.py
@@ -14,10 +14,10 @@ class Fotografia(models.Model):
         ("COMETA", "Cometa"),
     ]
 
-    nome = models.CharField(max_length=100, null=False, blank=False)
-    legenda = models.CharField(max_length=150, null=False, blank=False)
+    nome = models.CharField(max_length=100, blank=False)
+    legenda = models.CharField(max_length=150, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
-    descricao = models.TextField(null=False, blank=False)
+    descricao = models.TextField(blank=False)
     foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     publicada = models.BooleanField(default=True)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
